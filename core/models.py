2c1ac2772cfa12959648ea31589b39cd1acf143f
Donald Le <donald.le@iamondemand.com>
2022-10-19 18:02:10 +0700

    Fix default value of OrderItem

--- core/models.py
+++ core/models.py
@@ -69,7 +69,7 @@ class OrderItem(models.Model):
                              on_delete=models.CASCADE)
     ordered = models.BooleanField(default=False)
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=7)
+    quantity = models.IntegerField(default=1)
 
     def __str__(self):
         return f"{self.quantity} of {self.item.title}"
